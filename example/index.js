fab7dbbbf4df1554ae3d8a7029605609c40be7ea
Ionică Bizău <bizauionica@yahoo.com>
2015-06-05 13:03:04 +0300

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,14 @@
+// Dependencies
+var ParentSearch = require("../lib");
+
+// Search all package.json files starting with this example path
+ParentSearch(__dirname + "/path/to/some/directory", "package.json", {
+    multiple: true
+}, function (err, data) {
+    console.log(err || data);
+}, function (path, dirname) {
+    console.log("Found " + path.p + " in " + path.d);
+});
+
+// Search all package.json files starting with this example path
+console.log(ParentSearch(__dirname + "/path/to/some/directory", "package.json", { multiple: true }));
